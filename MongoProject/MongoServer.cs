de5f392f6fd259ba28b09b9d142cc0cc2866b003
G1577 <guyb932005@gmail.com>
2024-04-20 17:56:10 +0300

    Adding new mongo project -fixed

---
+++ MongoProject/MongoServer.cs
@@ -0,0 +1,26 @@
+﻿using MongoProject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using MongoDB.Driver;
+//using MongoDB.Bson;
+
+namespace MongoProject
+{
+    public static class MongoServer
+    {
+        private static string connectionString;
+        public static Level GetLevel(int index)
+        {
+            //var client = new MongoClient(connectionString);
+            //var collection = client.GetDatabase("SantaLostTheGifts").GetCollection<BsonDocument>("Level");
+            //var filter = Builders<BsonDocument>.Filter.Eq("index", index);
+            //var level = collection.Find(filter).First();
+            //Console.WriteLine(level);
+            //return null;
+
+        }
+    }
+}
