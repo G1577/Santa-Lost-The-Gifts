1741b53f0af48cbbabacf0f12560d5cf0b4c4f75
G1577 <guyb932005@gmail.com>
2024-04-20 17:58:40 +0300

    Adding mongo packages

--- MongoProject/MongoServer.cs
+++ MongoProject/MongoServer.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using MongoDB.Driver;
-//using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace MongoProject
 {
@@ -14,12 +14,12 @@ namespace MongoProject
         private static string connectionString;
         public static Level GetLevel(int index)
         {
-            //var client = new MongoClient(connectionString);
-            //var collection = client.GetDatabase("SantaLostTheGifts").GetCollection<BsonDocument>("Level");
-            //var filter = Builders<BsonDocument>.Filter.Eq("index", index);
-            //var level = collection.Find(filter).First();
-            //Console.WriteLine(level);
-            //return null;
+            var client = new MongoClient(connectionString);
+            var collection = client.GetDatabase("SantaLostTheGifts").GetCollection<BsonDocument>("Level");
+            var filter = Builders<BsonDocument>.Filter.Eq("index", index);
+            var level = collection.Find(filter).First();
+            Console.WriteLine(level);
+            return null;
 
         }
     }
